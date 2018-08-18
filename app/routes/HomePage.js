5b777aae74b2f4e637287073b6a0a096d1aa317b
bobaeb <panzz7876@naver.com>
2018-08-19 05:21:03 +0900

    save master

--- app/routes/HomePage.js
+++ app/routes/HomePage.js
@@ -14,26 +14,31 @@ class HomePage extends Component {
       <div>
 
           <div className='header'>
-                <h1>logo</h1>
+                <h1></h1>
                 <div className='m1'></div>
                 <div className='m2'></div>
           </div>
 
           <div className='middle'>
-               <h2>title</h2>
+               <div className='title'>
+                 <h2></h2>
+               </div>
                <div className='img'></div>
           </div>
 
           <div className='bottom'>
                <div className='b1'>
                   <h3>title2</h3>
-                  <div></div>
+                  <div className='content'></div>
                </div>
-
-               <div className='b2'></div>
-               <div className='b3'></div>
-               <div className='b4'></div>
-
+               <ul className='b-content'>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li className='p'></li>
+               </ul>
+                
                <div>
                  <p></p>
                </div>
--- app/styles/pageStyles.js
+++ app/styles/pageStyles.js
@@ -10,28 +10,68 @@ const style = css`
     position: relative;
   }
   .header h1{
-    width: 100px;
+    width: 120px;
     height: 30px;
     background-color: gray;
     position: absolute;
   }
   .m1 { 
-    width: 100px;
+    width: 120px;
     height: 30px;
     background-color: gray;
     position: absolute;
     left: 50%;
   }
   .m2 { 
-    width: 100px;
+    width: 120px;
     height: 30px;
     background-color: gray;
     position: absolute;
     right: 0;
   }
-
-
- 
+  .middle {
+    width: 960px;
+    height: 800px;
+    margin: 0 auto;
+  }
+  .title {
+    width: 50%;
+    height: 60px;
+    background-color: darkgray;
+  }
+  .img {
+    width: 100%;
+    height: 760px;
+    margin-top: 20px;
+    background-color: red;
+  }
+  .bottom {
+    width: 960px;
+    height: 400px;
+    margin: 0 auto;
+  }
+  .b1 {
+    width: 230px;
+    height: 100%;
+  }
+  .b1 h3 {
+    width: 100px;
+    height: 20px;
+    background-color: gray;
+  }
+  .b1 .content {
+    height: 230px;
+    background-color: lightgrey;
+  }
+.b-content li {
+    width: 230px;
+    height: 100%;
+    background-color: gray;
+}
+.b-content .p {
+   height: 10px;
+   background-color: darkgray;
+ }
 `
 
 export default style
