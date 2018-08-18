72759853b253d317992fe5b3cff09d949f56045b
박보배 <bobae@bagbobaeui-MacBook-Pro.local>
2018-08-18 17:49:03 +0900

    save master

--- app/routes/HomePage.js
+++ app/routes/HomePage.js
@@ -13,28 +13,11 @@ class HomePage extends Component {
     return (
       <div>
 
-        <div style={{ color: "#6b6161" }}>
-          <div
-            style={{
-              height: "100px",
-              backgroundColor: "#fcecd7",
-              borderRadius: "4px",
-              textAlign: "center",
-              paddingTop: "40px",
-              paddingBottom: "40px"
-            }}
-          >
-            <span
-              style={{
-                lineHeight: "20px",
-                backgroundColor: "#b8e986",
-                color: "#4a90e2"
-              }}
-            >
-              Hello Colorful World 🖍
-            </span>
-          </div>
-        </div>
+<div className='blue'>
+
+       Hello Colorful World 🖍
+</div>
+       
 
       </div>
     ); // eslint-disable-line
--- app/styles/pageStyles.js
+++ app/styles/pageStyles.js
@@ -3,6 +3,10 @@ import { css } from 'styled-components'
 const style = css`
   body {
   }
+  .blue {
+    background: blue;
+    color: wheat;
+  }
 `
 
 export default style
